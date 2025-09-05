7d7fd105240a9db2c8c1ae76887e1a58165b87bc
DilipBhutiya <dilip.bhutiya@techavidus.com>
2025-09-05 14:51:41 +0530

    Resolve the consumer count issue based on date format

--- src/main/java/com/app/kyc/repository/ConsumerRepository.java
+++ src/main/java/com/app/kyc/repository/ConsumerRepository.java
@@ -202,18 +202,18 @@ public interface ConsumerRepository
     List<DashboardObjectInterface> countDistinctByServiceProvider_IdInAndCreatedOnBetweenGroupByYearMonthDate(Collection<Long> ids, Date createdOnStart, Date createdOnEnd, int consumerStatus);
 
     @Query(value = "SELECT COUNT(*) FROM consumers c where c.service_provider_id IN (?1)"
-    		+ "AND STR_TO_DATE(c.created_on, '%d-%m-%Y') "
-    		+ "BETWEEN STR_TO_DATE(?2, '%Y-%m-%d %H:%i:%s') "
-    		+ "AND STR_TO_DATE(?3, '%Y-%m-%d %H:%i:%s')", nativeQuery = true)
+    		+ "AND STR_TO_DATE(c.created_on, '%Y-%m-%d') "
+    		+ "BETWEEN STR_TO_DATE(?2, '%Y-%m-%d') "
+    		+ "AND STR_TO_DATE(?3, '%Y-%m-%d')", nativeQuery = true)
     long getTotalConsumers(Collection<Long> ids, Date createdOnStart, Date createdOnEnd);
 
     @Query(value = "SELECT sp.name AS operatorName, COUNT(c.id) AS total " +
             "FROM consumers c " +
             "JOIN service_providers sp ON c.service_provider_id = sp.id " +
             "where c.service_provider_id IN (?1) " +
-            "AND STR_TO_DATE(c.created_on, '%d-%m-%Y') " +
-    		"BETWEEN STR_TO_DATE(?2, '%Y-%m-%d %H:%i:%s') " +
-    		"AND STR_TO_DATE(?3, '%Y-%m-%d %H:%i:%s') " +
+            "AND STR_TO_DATE(c.created_on, '%Y-%m-%d') " +
+    		"BETWEEN STR_TO_DATE(?2, '%Y-%m-%d') " +
+    		"AND STR_TO_DATE(?3, '%Y-%m-%d') " +
             "GROUP BY sp.name ", nativeQuery = true)
     List<Object[]> getConsumersPerOperator(Collection<Long> ids, Date createdOnStart, Date createdOnEnd);
 
