bfcd6981c46253cbf23546c2fff99ee1d6476372
DilipBhutiya <dilip.bhutiya@techavidus.com>
2025-09-10 17:29:38 +0530

    Resolve the issue of resolve vs unresolved and add a markAllAsRead API notification.

--- src/main/java/com/app/kyc/repository/NotificationRepository.java
+++ src/main/java/com/app/kyc/repository/NotificationRepository.java
@@ -2,7 +2,12 @@ package com.app.kyc.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.kyc.entity.Notification;
@@ -15,5 +20,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
    
    // Only unread notifications for a user
    List<Notification> findAllByUserIdAndMarkReadFalseOrderByCreatedOnDesc(Long userId);
+   
+   @Modifying
+   @Transactional
+   @Query("UPDATE Notification n SET n.markRead = 1 WHERE n.userId = :userId AND n.markRead = 0")
+   int markAllAsReadByUserId(@Param("userId") Long userId);
 
 }
