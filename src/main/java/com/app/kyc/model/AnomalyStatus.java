40e21fda9aed3326b4e694f8db064b85f35c3b6d
DilipBhutiya <dilip.bhutiya@techavidus.com>
2025-09-12 15:08:32 +0530

    Add a API of get all status and add a resolved Partially status and change the name of Resolve successfully to Resolve Fully

--- src/main/java/com/app/kyc/model/AnomalyStatus.java
+++ src/main/java/com/app/kyc/model/AnomalyStatus.java
@@ -1,9 +1,15 @@
 package com.app.kyc.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum AnomalyStatus {
 
    REPORTED(0, "Reported"), UNDER_INVESTIGATION(1, "In Progress"), QUESTION_SUBMITTED(2, "Question Submitted"), QUESTION_ANSWERED(3,
-      "Question Answered"), RESOLUTION_SUBMITTED(4, "Resolution Submitted"), RESOLVED_SUCCESSFULLY(5, "Resolved Successfully"), WITHDRAWN(6, "Withdrawn");
+      "Question Answered"), RESOLUTION_SUBMITTED(4, "Resolution Submitted"), RESOLVED_PARTIALLY(5, "Resolved Partially"),RESOLVED_SUCCESSFULLY(6, "Resolved Fully"), WITHDRAWN(7, "Withdrawn");
 
    private Integer code;
    private String status;
@@ -34,4 +40,15 @@ public enum AnomalyStatus {
       this.status = status;
    }
 
+   // ✅ Convert to list of maps
+   public static List<Map<String, Object>> asList() {
+       return Arrays.stream(values())
+               .map(s -> {
+                   Map<String, Object> map = new HashMap<>();
+                   map.put("code", s.getCode());
+                   map.put("status", s.getStatus());
+                   return map;
+               })
+               .collect(Collectors.toList());
+   }	
 }
