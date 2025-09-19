cdec341896ae181eb1f57c531cae0d5ddf4b79a9
Kumar.saragadam <Kumar.saragadam@itmaxglobal.com>
2025-09-19 14:33:51 +0530

    Kumar\CKYC-120\Introduce “Partially Resolved" consistent on duplicates

--- src/main/java/com/app/kyc/service/ConsumerServiceImpl.java
+++ src/main/java/com/app/kyc/service/ConsumerServiceImpl.java
@@ -10,6 +10,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
