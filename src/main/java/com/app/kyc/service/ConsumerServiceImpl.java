5a5c10a20b2a062230f53610a3f5995716e78162
Kumar Saragadam <kumar.saragadam@itmaxglobal.com>
2025-09-22 11:13:33 +0200

    Kumar\CKYC-120\Bug Fix for Exceeding Threshold Status

--- src/main/java/com/app/kyc/service/ConsumerServiceImpl.java
+++ src/main/java/com/app/kyc/service/ConsumerServiceImpl.java
@@ -2767,7 +2767,7 @@ System.out.println("Get all flagged ");
                                         anomaly,
                                         new Date(),
                                         newStatus,
-                                        "Anomaly fagged by system",
+                                        "Anomaly Resolved by system",
                                         user.getFirstName() + " " + user.getLastName(),
                                         anomaly.getUpdatedOn()
                                 )
